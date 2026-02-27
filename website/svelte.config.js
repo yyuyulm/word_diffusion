4f5a3412ed8d24a87a1571ad4374dd498be35fdb
yyuyulm <yyuyulm@gmail.com>
2026-02-26 23:37:51 -0500

    website deployment action and config

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -14,7 +14,7 @@ const config = {
             strict: true
         }),
         paths: {
-            base: '',
+            base: process.env.NODE_ENV === 'production' ? '/word_diffusion' : '',
         }
     }
 };
